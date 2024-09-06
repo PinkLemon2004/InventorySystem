71c9e243fc15215e57e34ea6b0fb2ccf2fc77511
RUJIRA NAVAEN <rujira.nava@bu.ac.th>
2024-09-07 06:45:36 +0700

    ff

--- Pages/Siam/CreateSiam.cshtml.cs
+++ Pages/Siam/CreateSiam.cshtml.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
 
-namespace inventorySystem.Pages.Rangsit
+namespace inventorySystem.Pages.Siam
 {
-    public class CreateRangsitModel : PageModel
+    public class CreateSiamModel : PageModel
     {
         public StockInfo stockInfo = new StockInfo();
         public string errorMessage = "";
@@ -32,7 +32,7 @@ namespace inventorySystem.Pages.Rangsit
 
             try
             {
-                string connectionString = "Server=tcp:inventory-lemon.database.windows.net,1433;Initial Catalog=inventory2;Persist Security Info=False;User ID=lemon436;Password=@sqlcs436;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; 
+                string connectionString = "Server=tcp:inventory-lemon.database.windows.net,1433;Initial Catalog=inventory2;Persist Security Info=False;User ID=lemon436;Password=@sqlcs436;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
@@ -62,7 +62,7 @@ namespace inventorySystem.Pages.Rangsit
             stockInfo.amount = "";
 
             successMessage = "New item added successfully!";
-            Response.Redirect("/Rangsit/IndexRangsit");
+            Response.Redirect("/Siam/IndexSiam");
         }
     }
 }
